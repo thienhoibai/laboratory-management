bdbf04345595b9c8c404767929413875de9797f1
Nguyen Dao Thien <nguyendaothien2003@gmail.com>
2025-10-23 09:20:45 +0700

    Reapply "IAMservice + patientserrvice + docker,rabbitq,grpc"
    This reverts commit 6e85f6bd24d95c627485a17bbc240d6a58a5b468.

---
+++ Laboratory_Management/Backend/LabolaryManagement/Contracts/Events/Patient/PatientCreatedV1.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts.Events.Patient;
+
+// Minimal integration event for demo. Can be extended later.
+public record PatientCreatedV1
+(
+    Guid PatientId,
+    Guid RequestedByUserId,
+    string Name,
+    DateTime CreatedAtUtc
+);
---
+++ Laboratory_Management/Backend/LabolaryManagement/Contracts/Events/Patient/PatientUpdatedV1.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts.Events.Patient;
+
+public record PatientUpdatedV1
+(
+    Guid PatientId,
+    Guid RequestedByUserId,
+    DateTime UpdatedAtUtc,
+    int ChangesCount
+);
