89c3521da2f6b9d094f7cfd9b89049be363c2a81
Nguyen Dao Thien <nguyendaothien2003@gmail.com>
2025-10-23 09:21:02 +0700

    Revert "IAMservice + patientserrvice + docker,rabbitq,grpc"
    This reverts commit b19a6ea93f348ec75eeefa1240b8ba7ff0a85d4d.

--- Laboratory_Management/Backend/LabolaryManagement/Patient.Application/Messaging/IPatientEventPublisher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Patient.Application.Messaging;
-
-public interface IPatientEventPublisher
-{
-    Task PublishPatientCreatedAsync(Guid patientId, Guid actorUserId, string? nameNorm, DateTime createdAtUtc, CancellationToken ct = default);
-    Task PublishPatientUpdatedAsync(Guid patientId, Guid actorUserId, DateTime updatedAtUtc, int changesCount, CancellationToken ct = default);
-    Task PublishPatientDeletedAsync(Guid patientId, Guid actorUserId, DateTime deletedAtUtc, CancellationToken ct = default);
-}
