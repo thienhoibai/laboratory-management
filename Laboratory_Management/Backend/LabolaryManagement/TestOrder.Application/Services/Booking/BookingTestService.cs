fa23bffe5c0da4ecae535cca1cccc929266de7c8
Ton Thanh VInh <bloomerrang@gmail.com>
2025-10-25 19:58:42 +0700

    Adding BookingService (In Progress)

---
+++ Laboratory_Management/Backend/LabolaryManagement/TestOrder.Application/Services/Booking/BookingTestService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestOrder.Infrastructure.Repository;
+
+namespace TestOrder.Application.Services.Booking
+{
+    public class BookingTestService
+    {
+        private readonly BookingTestRepository _bookingTestRepository;
+
+        public BookingTestService(BookingTestRepository bookingTestRepository)
+        {
+            _bookingTestRepository = bookingTestRepository;
+        }
+
+        public async Task<int> AddBookingTestAsync(long bookingId, long testId)
+        {
+           
+        }
+    }
+}
