40b05bccdaf41c9ba68dd02fb62ef8171187ce40
Ton Thanh VInh <bloomerrang@gmail.com>
2025-10-25 09:18:44 +0700

    Modify Database

--- Laboratory_Management/Backend/LabolaryManagement/TestOrder.Infrastructure/Models/TestParameter.cs
+++ Laboratory_Management/Backend/LabolaryManagement/TestOrder.Infrastructure/Models/TestParameter.cs
@@ -12,7 +12,6 @@ public partial class TestParameter
     public string? Unit { get; set; }
 
     public string? ReferenceRange { get; set; }
-    public int CatalogId { get; set; }
 
     public virtual ICollection<TestResult> TestResults { get; set; } = new List<TestResult>();
 }
