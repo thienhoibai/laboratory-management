90b5ea36bcf60eea2a63d5f073ca286d90bef261
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-11-04 11:08:44 +0700

    BlogService Base

---
+++ Laboratory_Management/Backend/LabolaryManagement/BlogService.Infrastructure/Models/Tag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlogService.Infrastructure.Models;
+
+public partial class Tag
+{
+    public int TagId { get; set; }
+
+    public string? TagName { get; set; }
+}
