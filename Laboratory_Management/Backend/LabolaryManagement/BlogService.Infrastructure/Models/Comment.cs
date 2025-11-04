458adddef29249009708de5d8c3f51eb6caf88f8
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-11-04 11:08:44 +0700

    BlogService Base

---
+++ Laboratory_Management/Backend/LabolaryManagement/BlogService.Infrastructure/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlogService.Infrastructure.Models;
+
+public partial class Comment
+{
+    public int CommentId { get; set; }
+
+    public int? PostId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? Content { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public bool? IsUpdated { get; set; }
+
+    public virtual BlogPost? Post { get; set; }
+}
