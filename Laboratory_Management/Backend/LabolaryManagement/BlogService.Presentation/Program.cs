5387ba2f62900a8328f7e949a9c44b83414526b6
MinhTuan <leminhtuansoftware@gmail.com>
2025-11-10 12:29:36 +0700

    update

--- Laboratory_Management/Backend/LabolaryManagement/BlogService.Presentation/Program.cs
+++ Laboratory_Management/Backend/LabolaryManagement/BlogService.Presentation/Program.cs
@@ -30,7 +30,21 @@ namespace BlogService.Presentation
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy
+                        .WithOrigins(
+                            "http://localhost:5174",
+                            "http://127.0.0.1:5174"
+                        )
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+
 
             var app = builder.Build();
 
@@ -40,10 +54,10 @@ namespace BlogService.Presentation
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseRouting();
             app.UseHttpsRedirection();
-
-            app.UseAuthorization();
+            app.UseCors("AllowFrontend");
+            app.UseAuthorization();s
 
 
             app.MapControllers();
