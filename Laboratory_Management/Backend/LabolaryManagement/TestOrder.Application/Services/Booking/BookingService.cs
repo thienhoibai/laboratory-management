c5d5a96073dae3e8a4f7467c0f1785e1bbc8bc4b
Ton Thanh VInh <bloomerrang@gmail.com>
2025-11-13 15:25:48 +0700

    New response for crud functions

--- Laboratory_Management/Backend/LabolaryManagement/TestOrder.Application/Services/Booking/BookingService.cs
+++ Laboratory_Management/Backend/LabolaryManagement/TestOrder.Application/Services/Booking/BookingService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Azure;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using TestOrder.Application.DTOs;
 using TestOrder.Application.DTOs.Bookings;
 using TestOrder.Infrastructure.Repository;
 
@@ -129,10 +131,16 @@ namespace TestOrder.Application.Services.Booking
         }
 
         #region Create New Booking
-        public async Task<int> CreateNewBooking(BookingRequestDTO bookingRequest)
+
+        public async Task<ResponseMessage> CreateBookingAsync (BookingRequestDTO bookingRequest)
         {
+            ResponseMessage response = new ResponseMessage();
             if (!_appointmentSlotService.IsAppointmentsDateValid(bookingRequest.slotDTO.AppointmentDate))
-                return -1;
+            {
+                response.ResponseCode = ResponseCode.BadInstanceState;
+                response.Message = "Date must be 1 day in the future";
+                return response;
+            }
 
 
 
@@ -141,11 +149,13 @@ namespace TestOrder.Application.Services.Booking
                     bookingRequest.slotDTO.TimeBlock))
             {
                 await _appointmentSlotService.AddAppointmentSlotAsync(bookingRequest.slotDTO);
-                
+
             }
             if (_appointmentSlotService.IsAppointmentSlotMaxedOut(bookingRequest.slotDTO))
             {
-                return -2;
+                response.ResponseCode = ResponseCode.BadInstanceState;
+                response.Message = "This slot is currently full";
+                return response;
             }
 
             var appointmentSlot = await _appointmentSlotService.GetAppointmentSlotByDateAndTimeAsync(
@@ -183,33 +193,73 @@ namespace TestOrder.Application.Services.Booking
                     _bookingTestService.AddBookingTestAsync(newBooking.BookingId, catalogId).Wait();
                 }
             }
-            return 0;
 
-        }
+            response.ResponseCode = ResponseCode.Success;
+            response.Message = "Booking Successfully";
+            response.InstancesCode = newBooking.BookingId;
+
+            return response;
+
+        } 
+
         #endregion
 
-        public async Task<int> CheckInBooking (Guid bookingId)
+        public async Task<ResponseMessage> CheckInBooking (Guid bookingId)
         {
             var booking =  await _bookingRepository.GetByIdAsync(bookingId);
+            ResponseMessage response = new ResponseMessage();
             if (booking == null)
-                return -1;
-            if (booking.Status != (byte?) BookingStatusEnum.Confirmed) return -2;
+            {
+                response.ResponseCode = ResponseCode.NotFound;
+                response.Message = "Booking not found";
+                response.InstancesCode = bookingId;
+                return response;
+            }
+                
+            if (booking.Status != (byte?)BookingStatusEnum.Confirmed)
+            {
+                response.ResponseCode = ResponseCode.BadInstanceState;
+                response.Message = "Booking is not in a state that allows check-in";
+                response.InstancesCode = bookingId;
+                return response;
+            }
             booking.Status = (byte?)BookingStatusEnum.CheckedIn;
             booking.RunDate = DateOnly.FromDateTime(DateTime.Now);
             await _bookingRepository.UpdateAsync(booking);
-            return 0;
+
+            response.ResponseCode = ResponseCode.Success;
+            response.Message = "Check-in successful";
+            response.InstancesCode = bookingId;
+
+            return response;
 
         }
 
-        public async Task<int> CheckOutBooking(Guid bookingId)
+        public async Task<ResponseMessage> CheckOutBooking(Guid bookingId)
         {
             var booking = await _bookingRepository.GetByIdAsync(bookingId);
+            ResponseMessage response = new ResponseMessage();
             if (booking == null)
-                return -1;
-            if (booking.Status != (byte?)BookingStatusEnum.InProgress) return -2;
-            booking.Status = (byte?)BookingStatusEnum.Completed;
+            {
+                response.ResponseCode = ResponseCode.NotFound;
+                response.Message = "Booking not found";
+                response.InstancesCode = bookingId;
+                return response;
+            }
+
+            if (booking.Status != (byte?)BookingStatusEnum.InProgress)
+            {
+                response.ResponseCode = ResponseCode.BadInstanceState;
+                response.Message = "Booking is not in a state that allows check-out";
+                response.InstancesCode = bookingId;
+                return response;
+            }
+        booking.Status = (byte?)BookingStatusEnum.Completed;
             await _bookingRepository.UpdateAsync(booking);
-            return 0;
+            response.ResponseCode = ResponseCode.Success;
+            response.Message = "Check-out successful";
+            response.InstancesCode = bookingId;
+            return response;
         }
     }
 }
