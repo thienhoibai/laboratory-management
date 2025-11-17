b22f2008861bb140476b51e65a4ac21c9bb01c65
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-11-17 23:00:29 +0700

    update post blog

--- Laboratory_Management/Backend/LabolaryManagement/BlogService.Infrastructure/Models/BlogPost.cs
+++ Laboratory_Management/Backend/LabolaryManagement/BlogService.Infrastructure/Models/BlogPost.cs
@@ -11,7 +11,7 @@ public partial class BlogPost
 
     public string? Content { get; set; }
 
-    public long? AuthorId { get; set; }
+    public Guid? AuthorId { get; set; }
 
     public int? CategoryId { get; set; }
 
