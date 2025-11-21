b446b16aba95e90367693c50d4a3e49973a16e9c
Hoang Anh Tuan <tuanhase180764@fpt.edu.vn>
2025-11-21 09:30:24 +0700

    search blog by title or categoryname

--- Laboratory_Management/Backend/LabolaryManagement/BlogService.Presentation/Controllers/BlogPostController.cs
+++ Laboratory_Management/Backend/LabolaryManagement/BlogService.Presentation/Controllers/BlogPostController.cs
@@ -22,13 +22,14 @@ namespace BlogService.Presentation.Controllers
         public async Task<IActionResult> GetAllBlogs(
     [FromQuery] Guid? authorId,
     [FromQuery] int? status,
+    [FromQuery] string? search,
     [FromQuery] int page = 1,
     [FromQuery] int pageSize = 10)
         {
-            var totalItems = await _service.GetAllWithCategoryAsync(authorId, status, 1, int.MaxValue)
+            var totalItems = await _service.GetAllWithCategoryAsync(authorId, status, 1, int.MaxValue,search)
                 .ContinueWith(t => t.Result.Count);
 
-            var data = await _service.GetAllWithCategoryAsync(authorId, status, page, pageSize);
+            var data = await _service.GetAllWithCategoryAsync(authorId, status, page, pageSize, search);
 
             return Ok(new
             {
